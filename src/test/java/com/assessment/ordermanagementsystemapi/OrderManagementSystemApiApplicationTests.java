25c138e2a1c453c4a69827249a0414001727a036
Liza <lixia101111@gmail.com>
2023-09-22 08:45:18 +0300

    Set up

---
+++ src/test/java/com/assessment/ordermanagementsystemapi/OrderManagementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.ordermanagementsystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
