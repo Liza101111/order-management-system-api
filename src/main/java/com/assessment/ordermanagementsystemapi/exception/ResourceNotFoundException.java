8740e745dab0fc49f071cdcc43ac42961573dbf0
Liza <lixia101111@gmail.com>
2023-09-25 09:51:22 +0300

    Add ResourceNotFoundException

---
+++ src/main/java/com/assessment/ordermanagementsystemapi/exception/ResourceNotFoundException.java
@@ -0,0 +1,5 @@
+package com.assessment.ordermanagementsystemapi.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message){ super(message); }
+}
